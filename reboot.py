3669b39c49036182748bb8d2b1276dbe4db24b86
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-09 00:19:08 +0530

    Python code to reboot the system

---
+++ reboot.py
@@ -0,0 +1,6 @@
+# this is included to run this file on terminal of linux
+#! /usr/bin/python3  
+# OS library is used in python to interface python and OS like windows, linux, etc
+import os
+#this command will reboot the system
+os.system('reboot')   
