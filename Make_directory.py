efdb1b9ded17d50a56d5cbbdb790a33ba4e82b49
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-10 22:24:54 +0530

    Create Directories
    Make directories in OS by giving a path, number of directories and a basic name

--- Make_directory.py
+++ Make_directory.py
@@ -5,6 +5,13 @@ no_of_dir=int(input("Enter no of directories"))
 basic_name=input("Enter basic name of Directory")
 
 os.chdir(path)
-for i in range(no_of_dir):
-    command='mkdir '+basic_name+str(i)
-    os.system('mkdir '+basic_name+str(i))
+if len(basic_name)<1:
+    basic_name=" "
+    print("Directory name is small")
+else:    
+    for i in range(no_of_dir):
+        command='mkdir '+basic_name+str(i)
+        os.system('mkdir '+basic_name+str(i))
+print ("Directories created")      
+
+
