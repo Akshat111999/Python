e31765a138a1125863e7356f24645d7f45db4c34
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-11 14:56:56 +0530

    problem 5 completed

---
+++ problem5.py
@@ -0,0 +1,14 @@
+import datetime
+user_name=input("Enter your name:")
+Enter your name:bhavya_vijay
+now=datetime.datetime.now()
+this_time=now.hour
+
+if this_time in range(6,12):
+      print("Good Morning...{}".format(user_name))
+elif this_time in range(12,16):
+      print("Good Afternoon...{}".format(user_name))
+elif this_time in range(16,20):
+      print("Good Evening...{}".format(user_name))
+else :
+      print("Good Night...{}".format(user_name))
