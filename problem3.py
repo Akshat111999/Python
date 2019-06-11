fce29914fe60df4f8ef1e8de9b2bab098cee8afe
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-11 11:51:07 +0530

    problem3 completed

---
+++ problem3.py
@@ -0,0 +1,35 @@
+#take a list say  adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+#write the program that will do
+#i)  print only those numbers greater than 5
+#ii)  also print those numbers those are less than or  equals to 2  ( <= 2 )
+
+iii)  store these answers in in two different list also
+
+
+
+
+
+
+
+
+
+
+adhoc = [1,2,3,1,4,5,66,22,2,6,0,9]
+for i in adhoc:
+	if i>5:
+		print(i)
+
+
+
+for i in adhoc:
+	if i<=2:
+
+		print(i)
+
+
+
+a = [ i for i in adhoc   if i<=2]
+print(a)
+
+b = [i for i in adhoc    if i>5]
+print(b)
