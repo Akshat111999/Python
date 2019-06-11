539b4c8f24bf9d86b505bebdf335ef06d5a89e98
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-11 11:45:00 +0530

    problem1 completed

---
+++ problem1.py
@@ -0,0 +1,9 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message that will tell them the year that they will turn 95 years old.
+
+import time
+name=input("enter your name")
+age=int(input("enter your age"))
+
+years=95-age
+print(f"your name is {name} and age {age}, you will turn 95 in year ",time.localtime().tm_year+years)
