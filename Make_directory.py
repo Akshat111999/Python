6f1872a2fa8845ebbbb2daa7af69e950cecd0f9e
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-14 23:50:12 +0530

    Python code to create directories

--- Make_directory.py
+++ Make_directory.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 import os
 
 path=input("Enter the path")
@@ -10,8 +11,8 @@ if len(basic_name)<1:
     print("Directory name is small")
 else:    
     for i in range(no_of_dir):
-        command='mkdir '+basic_name+str(i)
         os.system('mkdir '+basic_name+str(i))
+
 print ("Directories created")      
 
 
