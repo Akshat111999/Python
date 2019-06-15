a597b110e2743dbf7dcc8d79ab60a68232b7f027
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-16 00:31:29 +0530

    problem1 completed

--- problem01.py
+++ problem01.py
@@ -3,7 +3,7 @@
 
 import time
 name=input("enter your name")
-age=int(input("enter your age"))
+age=int(input("enter your age: "))
 
 years=95-age
 print(f"your name is {name} and age {age}, you will turn 95 in year ",time.localtime().tm_year+years)
