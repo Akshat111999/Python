c212226636b3d1c080387cb27d6974abf153a4af
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-15 23:46:33 +0530

    problem4 completed

--- problem4.py
+++ problem4.py
@@ -1,12 +1,14 @@
 import os
-user=input()
+import crypt
+print("Username should contain only letters or combination of letters and numbers")
+user=input("Enter username: ")
 if( user.isdigit()):
     print("User input is Number ")
 elif "." in user:
-    print("number")
+    print("User input is a number")
 else:
-    print("User input is string ")
-    os.system("useradd -p "+ user)
-    password ="hello" + user 
-
-print("user created " + user)
+    #print("User input is string ")
+    password ="hello" + user
+    encpass=crypt.crypt(password,'123')
+    os.system('sudo useradd -p '+ encpass +' '+user)
+    print("user created " + user)
