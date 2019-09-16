6c8df802b8bb7c53fe391601ee32994bba05d69d
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-09-16 23:59:42 +0530

    create your first label

--- GUI/labels.py
+++ GUI/labels.py
@@ -1,4 +1,6 @@
 from tkinter import *
-root = Tk()
-Label(root, text="First Label").pack()
-root.mainloop()
+root = Tk()  #creates a root window 
+
+Label(root, text="First Label").pack()  #.pack() method is used to organise the blocks in rows and columns
+
+root.mainloop() # it is an infinite loop and this method is used to run the application
